7a7e466166ad90570b2eed3bd07dd1701864c776
Sunny <77973332+sunny080@users.noreply.github.com>
2021-12-16 02:39:06 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("mySidebar").style.marginRight = "0px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.marginRight  = "730px";
+}
